d160e924ea5733da19a000335d3d3fb0b7636ecb
行者 <285779289@qq.com>
2015-06-26 18:50:32 +0800

    后台拼接json串时， 一定要注意， 所有的Key一定要用双引号引住， 所有的字符串也一定要用双引号引住， 采用ajaxForm异步提交form.

--- src/sfs/controller/controller.go
+++ src/sfs/controller/controller.go
@@ -950,7 +950,7 @@ func SaveQ7EditDetail(w http.ResponseWriter, r *http.Request, ps httprouter.Para
 	
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	fmt.Fprintf(w, "{errcode:%d ,errmsg:%s}", 0 ,"Success")
+	fmt.Fprintf(w, "{\"errcode\":%d, \"errmsg\": \"%s\",\"id\":\"%s\"}", 0 ,"Success", "1001")
 	
 }
 //admin end
